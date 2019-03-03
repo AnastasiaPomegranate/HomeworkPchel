279363928329c70c428a6054c20c8a9ccb4f8ef8
AnastasiaPomegranate <43775945+AnastasiaPomegranate@users.noreply.github.com>
2019-03-03 20:50:59 +0300

    Update 2ex.py

--- 1lesson/2ex.py
+++ 1lesson/2ex.py
@@ -9,14 +9,14 @@ def nahodX(A,B,C):
 		if D>0:
 			X1=(-B+math.sqrt(D))/(2*A)
 			X2=(-B-math.sqrt(D))/(2*A)
-			print("%.2f, %.2f"%(X1,X2))
+			print("%.2f, %.2f"%(X1 , X2))
 		elif D==0 :
 			X1=-B/(2*A)
 			print("%.2f"%(X1))
 		else :
-			print ("Корней нет")
+			print (" ")
 	elif (B==0 and A==0):
-		print("Корней нет")
+		print(" ")
 	else:
 		X1=-C/B
 		print("%.2f"%(X1))
